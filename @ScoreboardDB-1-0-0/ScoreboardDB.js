eebf50ea87c3b888a97ffc8f28c22eb9a0a4e330
Blackstrat67 <143889285+Blackstrat67Official@users.noreply.github.com>
2026-03-01 16:51:02 +0100

    Add export for DataRelations in ScoreboardDB.js

--- @ScoreboardDB-1-0-0/ScoreboardDB.js
+++ @ScoreboardDB-1-0-0/ScoreboardDB.js
@@ -1,4 +1,5 @@
 export { CacheManager } from "./CacheManager";
 export { DataFormat } from "./DataFormat";
+export { DataRelations } from "./DataRelations";
 export { DataSchema } from "./DataSchema";
 export { ScoreboardStorage } from "./ScoreboardStorage";
